52ec59c83d6f6af99bb218d0ba33c07ba7ccbbc3
Esteban Hernandez <eshernan@gmail.com>
2021-04-24 05:31:28 -0500

    minor documentation on script

--- intel/compiling.sh
+++ intel/compiling.sh
@@ -15,16 +15,28 @@ export CXX=icpc
 # export CXX=g++
 source ~/.bash_intel20
 cd $INSTALLERS
+echo "-------------------------------------------------"
+echo "----------- Compiling ZLIB  ---------------------"
+echo "-------------------------------------------------"
+
 cd zlib*
 ./configure --prefix=$LIBS/zlib
 make
 make install
 cd ..
+echo "-------------------------------------------------"
+echo "----------- Compiling  LIBPNG  ------------------"
+echo "-------------------------------------------------"
+
 cd libpng*
 ./configure --prefix=$LIBS/libpng #--build=arm
 make
 make install
 cd ..
+echo "-------------------------------------------------"
+echo "----------- Compiling JASPER  -------------------"
+echo "-------------------------------------------------"
+
 cd jasper*
 ./configure --prefix=$LIBS/jasper
 make
@@ -44,21 +56,37 @@ export CFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 export CXXFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 export FFLAGS='-O3 -xHost -ip -no-prec-div -static-intel'
 # export CXXFLAGS=-03
+
+echo "-------------------------------------------------"
+echo "----------- Compiling HDF5  ---------------------"
+echo "-------------------------------------------------"
+
  cd hdf5*
 ./configure --prefix=$LIBS/hdf5 --enable-parallel --enable-fortran  --enable-cxx  --enable-optimization=high --with-default-api-version=v18
 make
 make install
 cd ..
+echo "-------------------------------------------------"
+echo "----------- Compiling PNETCDF  ------------------"
+echo "-------------------------------------------------"
+
 cd pnetcdf*
 CPPFLAGS="-I$LIBS/hdf5/include/" LDFLAGS=-L$LIBS/hdf5/lib/ ./configure --prefix=$LIBS/pnetcdf 
 make
 make install
 cd ..
+echo "-------------------------------------------------"
+echo "----------- Compiling NETCDF-C  -----------------"
+echo "-------------------------------------------------"
 cd netcdf-c*
 CPPFLAGS="-I$LIBS/hdf5/include/ -I$LIBS/pnetcdf/include" LDFLAGS="-L$LIBS/hdf5/lib/ -L$LIBS/pnetcdf/lib" ./configure --prefix=$LIBS/netcdf --enable-netcdf-4 --enable-pnetcdf
 make
 make install
 cd ..
+echo "-------------------------------------------------"
+echo "----------- Compiling NETCDF-FORTRAN  -----------"
+echo "-------------------------------------------------"
+
 cd netcdf-f*
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBS/netcdf/lib CPPFLAGS="-I$LIBS/hdf5/include/ -I$LIBS/netcdf/include" LDFLAGS="-L$LIBS/hdf5/lib/ -L$LIBS/netcdf/lib" ./configure --prefix=$LIBS/netcdf 
 make
