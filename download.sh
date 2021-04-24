ab6ab245db75b0ecf977b4413137180264cd2dbe
Esteban Hernandez <eshernan@gmail.com>
2021-04-23 20:08:38 -0500

    changes to pnet1.12 and netcdf-c 4.8

--- download.sh
+++ download.sh
@@ -15,7 +15,7 @@ git clone https://bitbucket.org/icl/papi.git
 tar -zxvf netcdf-c-4.7.4.tar.gz
 tar -zxvf netcdf-fortran-4.5.2.tar.gz
 tar -zxvf hdf5-1.12.0.tar.gz
-tar -zxvf jasper-1.900.1.tar.gz
+tar -zxvf jasper-1.900.1.tar.gz 
 tar -zxvf libpng-1.2.50.tar.gz
 tar -zxvf zlib-1.2.7.tar.gz
 tar -zxvf mpich-3.0.4.tar.gz
