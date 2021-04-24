9df106030f17a3c1fd60e3545e4b252a9671362c
Esteban Hernandez <eshernan@gmail.com>
2021-04-23 21:43:06 -0500

    ENV Variables introduced to easy installation

--- intel/compiling.sh
+++ intel/compiling.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ../download.sh
 SHARED=/home/ehernandez/WRF_BASE
+LIBS=$SHARED/libs
 INSTALLERS=$SHARED/installer
 export SHARED INSTALLERS
 #for Intel Cluster Compilers
@@ -16,55 +17,55 @@ export CXX=icpc
 
 cd $INSTALLERS
 cd zlib*
-./configure --prefix=$SHARED/libs/zlib
+./configure --prefix=$LIBS/zlib
 make
 make install
 cd ..
 cd lipng*
-./configure --prefix=$SHARED/libs/libpng #--build=arm
+./configure --prefix=$LIBS/libpng #--build=arm
 make
 make install
 cd ..
 cd jasper*
-./configure --prefix=$SHARED/libs/jasper
+./configure --prefix=$LIBS/jasper
 make
 make install
 cd ..
-cd mpich*
-./configure --prefix=$SHARED/libs/mpich --with-slurm=/usr/lib64/slurm --enable-shared
-make
-make install
-cd ..
-export  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SHARED/libs/mpich/lib
-export PATH=$PATH:$SHARED/libs/mpich/bin
-cd hdf5*
-./configure --prefix=$SHARED/libs/hdf5 --enable-parallel --enable-fortran --enable-shared
+# cd mpich*
+# ./configure --prefix=$SHARED/libs/mpich --with-slurm=/usr/lib64/slurm --enable-shared
+# make
+# make install
+# cd ..
+# export  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SHARED/libs/mpich/lib
+# export PATH=$PATH:$SHARED/libs/mpich/bin
+# cd hdf5*
+./configure --prefix=$LIBS/hdf5 --enable-parallel --enable-fortran --enable-shared
 make -j 20
 make install
 cd ..
 cd pnetcdf*
-CPPFLAGS="-I$SHARED/libs/hdf5/include/ -I$SHARED/libs/mpich/include" LDFLAGS=-L$SHARED/libs/hdf5/lib/ ./configure --prefix=$SHARED/libs/pnetcdf --enable-shared
+CPPFLAGS="-I$LIBS/hdf5/include/ -I$LIBS/mpich/include" LDFLAGS=-L$$LIBS/hdf5/lib/ ./configure --prefix=$LIBS/pnetcdf --enable-shared
 make
 make install
 cd ..
 cd netcdf-c*
-CPPFLAGS="-I$SHARED/libs/hdf5/include/ -I$SHARED/libs/mpich/include -I$SHARED/libs/pnetcdf/include" LDFLAGS="-L$SHARED/libs/hdf5/lib/ -L$SHARED/libs/pnetcdf/lib" ./configure --prefix=$SHARED/libs/netcdf --enable-netcdf-4 --enable-pnetcdf
+CPPFLAGS="-I$LIBS/hdf5/include/ -I$LIBS/mpich/include -I$LIBS/pnetcdf/include" LDFLAGS="-L$LIBS/hdf5/lib/ -L$LIBS/pnetcdf/lib" ./configure --prefix=$LIBS/netcdf --enable-netcdf-4 --enable-pnetcdf
 make
 make install
 cd ..
 cd netcdf-f*
-FC=mpif90 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SHARED/libs/netcdf/lib CPPFLAGS="-I$SHARED/libs/hdf5/include/ -I$SHARED/libs/netcdf/include" LDFLAGS="-L$SHARED/libs/hdf5/lib/ -L$SHARED/libs/netcdf/lib" ./configure --prefix=$SHARED/libs/netcdf --enable-shared
+FC=mpif90 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBS/netcdf/lib CPPFLAGS="-I$LIBS/hdf5/include/ -I$LIBS/netcdf/include" LDFLAGS="-L$LIBS/hdf5/lib/ -L$LIBS/netcdf/lib" ./configure --prefix=$LIBS/netcdf --enable-shared
 make
 make install
 cd ..
-cd papi
-./configure --prefix=$SHARED/libs/papi
-make
-make install
-cd ..
-export NETCDF=$SHARED/libs/netcdf/
-export HDF5=$SHARED/libs/hdf5
-export PHDF5=$SHARED/libs/hdf5
-export JASPERLIB=$SHARED/libs/jasper/lib
-export JASPERINC=$SHARED/libs/jasper/include
-export PATH=$PATH:$SHARED/libs/netcdf/bin/
+# cd papi
+# ./configure --prefix=$SHARED/libs/papi
+# make
+# make install
+# cd ..
+export NETCDF=$LIBS/netcdf/
+export HDF5=$LIBS/hdf5
+export PHDF5=$LIBS/hdf5
+export JASPERLIB=$LIBS/jasper/lib
+export JASPERINC=$LIBS/jasper/include
+export PATH=$PATH:$LIBS/netcdf/bin/
