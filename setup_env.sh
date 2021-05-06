1246eca9878c20ad516b7ee5533265350355d6d2
Esteban Hernandez <eshernan@gmail.com>
2021-05-05 19:14:02 -0500

    Integrate aws s3 bucket to download the GFS Data

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,8 @@
-export SHARED=/home/ehernandez/WRF_BASE
+export SHARED=/Users/awseste/source/wrf
 export LIBS=$SHARED/libs
 export INSTALLERS=$SHARED/installer
 export INSTALLER_PATH=$INSTALLERS
 export GEO_STATIC_DATA=$SHARED/GEO_STATIC_DATA
 export DOMAIN_NAME=MyDOMAIN
+export GFS_DATA=$DOMAIN_NAME/GFS_DATA
+export GFS_PLEVEL=p25
