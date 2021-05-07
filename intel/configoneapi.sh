45d19c0fdff7e407843730a6dfde21962854d0e0
eshernan@gmail.com <eshernan@gmail.com>
2021-05-07 15:00:22 -0500

    Change the filename with blankspaces

--- intel/configoneapi.sh
+++ intel/configoneapi.sh
@@ -3,10 +3,10 @@
 unset LD_LIBRARY_PATH
 export BASE_COMPILER=/opt/intel/oneapi
 source $BASE_COMPILER/mkl/latest/env/vars.sh intel64
-source $BASE_COMPILER/inspector/latest/env/inspxe-vars.sh intel64
+source $BASE_COMPILER/inspector/latest/env/vars.sh intel64
 source $BASE_COMPILER/vtune/latest/env/vars.sh intel64
 source $BASE_COMPILER/advisor/latest/env/vars.sh intel64
-source $BASE_COMPILER/compilers/latest/env/vars.sh intel64
+source $BASE_COMPILER/compiler/latest/env/vars.sh intel64
 source $BASE_COMPILER/mpi/latest/env/vars.sh intel64
 
 
