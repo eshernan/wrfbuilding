78ed499270a813fc824a1f846e8520e5fc9a45e2
Esteban Herna´ndez ejhernandezb@udistrital.edu.co () <ejhernandezb@login01.hpcadvisorycouncil.com>
2021-10-07 10:37:40 -0700

    Changes to include local installation without root privileges

---
+++ intel/installOneAPI_local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+# Using this installer for use locally without root prvileges
+#
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18211/l_HPCKit_p_2021.4.0.3347_offline.sh
+sudo bash l_HPCKit_p_2021.4.0.3347_offline.sh
