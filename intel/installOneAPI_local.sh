dfbdfecdc335512b678f9d15ded9018e5c2dcc8a
Esteban Hernández <eshernan@gmail.com>
2023-02-22 07:53:36 -0500

    Update with 2023 version

--- intel/installOneAPI_local.sh
+++ intel/installOneAPI_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 # Using this installer for use locally without root prvileges
 #
-wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18211/l_HPCKit_p_2021.4.0.3347_offline.sh
-sudo bash l_HPCKit_p_2021.4.0.3347_offline.sh
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/19084/l_HPCKit_p_2023.0.0.25400_offline.sh
+sudo sh ./l_HPCKit_p_2023.0.0.25400_offline.sh
